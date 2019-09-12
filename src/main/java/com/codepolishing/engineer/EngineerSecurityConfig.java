c8cab6c01ccf0ad71ed26d80bbf2355ad23f8657
Mateusz <conju89@gmail.com>
2019-09-12 23:37:50 +0200

    pełne logowanie i rejestracja

---
+++ src/main/java/com/codepolishing/engineer/EngineerSecurityConfig.java
@@ -0,0 +1,70 @@
+package com.codepolishing.engineer;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+
+@Configuration
+@EnableWebSecurity
+public class EngineerSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+
+        auth.inMemoryAuthentication()
+                .withUser("mateusz").password(passwordEncoder().encode("qwerty")).roles("ADMIN")
+                .and()
+                .withUser("ola").password(passwordEncoder().encode("qwerty")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        String[] staticResources  =  {
+                "/css/**",
+                "/images/**",
+                "/fonts/**",
+                "/scripts/**",
+                "/scss/**",
+                "/js/**",
+                "../templates/**"
+        };
+
+        http.authorizeRequests()
+                .antMatchers("/", "/index","/sign_up","/sign_up2").permitAll()
+                .antMatchers(staticResources).permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                    .loginPage("/signIn")
+                    .failureUrl("/signInError")
+                    .permitAll()
+                .and()
+                .logout()
+                    .logoutUrl("/logout")
+                    .logoutSuccessUrl("/signIn")
+                    .permitAll();
+
+
+    }
+
+    @Bean
+    public AuthenticationSuccessHandler successHandler() {
+        SimpleUrlAuthenticationSuccessHandler handler = new SimpleUrlAuthenticationSuccessHandler();
+        handler.setUseReferer(true);
+        return handler;
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/com/codepolishing/engineer/controller/LoginController.java
@@ -0,0 +1,29 @@
+package com.codepolishing.engineer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class LoginController {
+
+    @RequestMapping("/signIn")
+    public String userLoginPage()
+    {
+        return  "user_login_page";
+    }
+
+    @RequestMapping("/signInError")
+    public String userLoginPageError(Model model)
+    {
+        model.addAttribute("loginError", true);
+        return "user_login_page";
+    }
+    @RequestMapping("/logout")
+    public String userLogoutPage()
+    {
+        return "user_login_page";
+    }
+}
