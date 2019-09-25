1766f5733cf9ac07cc8432e7bff7bd1c389906a5
DevPiotr <32850164+DevPiotr@users.noreply.github.com>
2019-09-25 16:04:57 +0200

    Working button in Courses,

--- src/main/java/com/codepolishing/engineer/controller/CourseController.java
+++ src/main/java/com/codepolishing/engineer/controller/CourseController.java
@@ -1,10 +1,12 @@
 package com.codepolishing.engineer.controller;
 
+import com.codepolishing.engineer.entity.Course;
 import com.codepolishing.engineer.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -20,5 +22,15 @@ public class CourseController {
         model.addAttribute("courses",courseRepository.findAll());
 
         return "courses";
+    }
+
+    @GetMapping("/{id}")
+    public String showOneCourse(@PathVariable("id")int id, Model model){
+
+        Course course = courseRepository.getOne(id);
+
+        model.addAttribute("course",course);
+
+        return "single_course";
     }
 }
