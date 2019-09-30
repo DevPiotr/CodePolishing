2503f8edc5aa9e5aa55d94abbf755584e1813c39
piotrdev <piotr.zukiewicz@o2.pl>
2019-09-30 15:46:39 +0200

    Changes view in Courses Courses in Menu direct user to courses view

--- src/main/java/com/codepolishing/engineer/controller/CourseController.java
+++ src/main/java/com/codepolishing/engineer/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.codepolishing.engineer.controller;
 
 import com.codepolishing.engineer.entity.Course;
+import com.codepolishing.engineer.repository.CourseLevelRepository;
 import com.codepolishing.engineer.repository.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,17 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 @RequestMapping("/courses")
 public class CourseController {
 
     @Autowired
     CourseRepository courseRepository;
+    @Autowired
+    CourseLevelRepository courseLevelRepository;
 
     @GetMapping("/")
     public String showCourses(Model model){
 
-        model.addAttribute("courses",courseRepository.findAll());
+        model.addAttribute("basicCourses",courseRepository.findCoursesByIdCourseLevel(1));
+        model.addAttribute("mediumCourses",courseRepository.findCoursesByIdCourseLevel(2));
+        model.addAttribute("hardCourses",courseRepository.findCoursesByIdCourseLevel(3));
+
+        model.addAttribute("courseLevelName",courseLevelRepository.findAll());
 
         return "courses";
     }
--- src/main/java/com/codepolishing/engineer/repository/CourseRepository.java
+++ src/main/java/com/codepolishing/engineer/repository/CourseRepository.java
@@ -3,5 +3,8 @@ package com.codepolishing.engineer.repository;
 import com.codepolishing.engineer.entity.Course;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CourseRepository extends JpaRepository<Course,Integer> {
+    List<Course> findCoursesByIdCourseLevel(int idCourseLevel);
 }
